ca682ac5e2cff7bf914c00a2acd1473f3bd927a2
jasonsanghb <670035399@qq.com>
2020-05-20 14:53:45 +0800

    完成了top的功能

---
+++ js/index.js
@@ -0,0 +1,31 @@
+$(function() {
+    // $('.location').on('mouseenter',function() {
+    //
+    //     $(this).children('.fn-hide').stop(true,false).slideDown(1000)
+    //
+    // })
+    // $('.location').on('mouseleave',function() {
+    //     $(this).children('.fn-hide').stop(true,false).slideUp(1000)
+    // })
+    // // 问题：为什么mouseenter web-nav时动画1s效果没有显现，而是很快地显示
+    // // 而且离开web-nav时 隐藏的元素是过了1s后马上消失，而不是在1s之内慢慢消失
+    // $('.web-nav').on('mouseenter',function() {
+    //     $(this).children('.fn-hide').stop(true,false).show(1000)
+    // })
+    // $('.web-nav').on('mouseleave',function() {
+    //     $(this).children('.fn-hide').stop(true,false).hide(1000)
+    // })
+    // $('.client-service').on('mouseenter',function() {
+    //     $(this).children('.fn-hide').stop(true,false).fadeIn(1000)
+    // })
+    // $('.client-service').on('mouseleave',function() {
+    //     $(this).children('.fn-hide').stop(true,false).fadeOut(1000)
+    // })
+    // $('.focus_JD').on('mouseenter',function() {
+    //     $(this).children('.fn-hide').stop(true,false).show()
+    // })
+    // $('.focus_JD').on('mouseleave',function() {
+    //     $(this).children('.fn-hide').stop(true,false).hide()
+    // })
+    
+})
